dc81c8d1ac94ef181e70de86ebe14880dd6db986
Naod Taddese <naodtds@gmail.com>
2024-01-10 22:56:15 +0300

    create base model

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+class base model
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    this class is blue print class
+    initializes id, created_at, updated_at
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+	initialize id, created_at, updated_at
+	"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """string representation"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def to_dict(self):
+        """dictionary representation of the object"""
+        obj = self.__dict__.copy()
+        obj["__class__"] = self.__class__.__name__
+        obj["created_at"] = obj["created_at"].isoformat()
+        obj["updated_at"] = obj["updated_at"].isoformat()
+        return obj
+
+    def save(self):
+        """save the object"""
+        self.updated_at = datetime.now()
