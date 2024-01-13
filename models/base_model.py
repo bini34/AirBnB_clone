9da232a7c8790e48d42f2b33588c375888cc0dab
Naod Taddese <naodtds@gmail.com>
2024-01-13 12:21:58 +0300

    fix style issues

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ class base model
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     this class is blue print class
@@ -12,16 +13,15 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """
-	initialize id, created_at, updated_at
-	"""
+        """initialize id, created_at, updated_at"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """string representation"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return ("[{}] ({}) {}"
+                .format(self.__class__.__name__, self.id, self.__dict__))
 
     def to_dict(self):
         """dictionary representation of the object"""
