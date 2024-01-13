e178174affadb0d4b9871f9a65f1c08e36646cb4
Naod Taddese <naodtds@gmail.com>
2024-01-13 23:01:45 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -16,9 +16,7 @@ class BaseModel:
         """initialize id, created_at, updated_at"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                else:
+                if key != '__class__':
                     if (key == "created_at" or key == "updated_at"):
                         val = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                         setattr(self, key, val)
