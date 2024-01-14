90b1de6c2293a7a8fdaa0694df4f0789671393f3
Naod Taddese <naodtds@gmail.com>
2024-01-14 17:14:17 +0300

    create

--- console.py
+++ console.py
@@ -2,13 +2,14 @@
 """
 Console module for the command interpreter.
 """
-
+from models.base_model import BaseModel
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     """command line intepretrer for airbnb"""
     prompt = "(hbnb) "
+    types = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -22,6 +23,17 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing on an empty line"""
         pass
 
+    def do_create(self, lines):
+        """this creates instance with create command """
+        if lines == "":
+            print("** class name missing **")
+            return
+        elif lines in self.types:
+            instance = eval(lines)()
+            instance.save()
+            print(instance.id)
+        else:
+            print("** class doesn't exit **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
